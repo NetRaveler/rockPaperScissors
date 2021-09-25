8b7f29aac0ea45fa29a5c0ebb391660dfc0b21ae
john <netraveler89@gmail.com>
2021-09-25 18:59:11 -0400

    add comments to main.js and complete game logic

--- scripts/main.js
+++ scripts/main.js
@@ -2,51 +2,56 @@
 let playerSelection = '';
 let computerSelection = '';
 let result = '';
+let round = 0;
+// get the user input and store it in a variable, return that variable
 function humanPlay() {
 	let answer = prompt("type Rock, Paper, or Scissors");
 	playerSelection = answer.toLowerCase();
 	console.log(playerSelection);
 	return(playerSelection);
 }
+// Randomly select the computer answer and store it in a variable
 function computerPlay() {
 	let randomNumber = Math.floor(Math.random() * 3);
 	if (randomNumber === 0) {
-		computerSelection = 'Rock';
+		computerSelection = 'rock';
 		return(computerSelection);
 	} else if (randomNumber === 1) {
-		computerSelection = 'Paper';
+		computerSelection = 'paper';
 		return(computerSelection);
 	}
 	else {
-		computerSelection = 'Scissors';
+		computerSelection = 'scissors';
 		return(computerSelection);
 	}
-	console.log(computerSelection);
 }
-
-function game(playerSelection, computerSelection) {
-	if (playerSelection === 'rock' && computerSelection === 'Scissors') {
+// Compare user and computer variables and determine outcome of the game
+function playRound() {
+	humanPlay();
+	computerPlay();
+	if (playerSelection === 'rock' && computerSelection === 'scissors') {
 		result = 'You Win! ' + playerSelection + ' beats ' + computerSelection;
 		return(result);
-	} else if (playerSelection === 'paper' && computerSelection === 'Rock') {
-
-		result = 'You Win! ' + playerSelection + ' beats ' + computerSelection;
-		return(result);
-	} else if (playerSelection === 'scissors' && computerSelection === 'Paper') {
+	} else if (playerSelection === 'paper' && computerSelection === 'rock') {
 
 		result = 'You Win! ' + playerSelection + ' beats ' + computerSelection;
 		return(result);
-	} /*else if (playerSelection !== 'rock' || playerSelection !== 'paper' || playerSelection !== 'scissors') {
-		alert('You must choose Rock, Paper, or Scissors'); }*/	
+	} else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+
+		result = 'You Win! ' + playerSelection + ' beats ' + computerSelection;
+		return(result);
+	} else if (playerSelection === computerSelection) {
+		result = "It's a Tie!"
+	}
 	else {
 		result = 'You Lose! ' + computerSelection + ' beats ' + playerSelection;
 		return(result);
 	}
 }
-
-function runIt() {
-	computerPlay();
-	humanPlay();
-	game(playerSelection, computerSelection);
+// Play 5 rounds of the game
+function game() {
+	for (round = 0; round < 5; round++) {
+	playRound();
 	alert(result);
+	}
 }
