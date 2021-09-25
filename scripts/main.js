7f69b71a6d43a274f7df7fe5ce44be8489fc07c2
john <netraveler89@gmail.com>
2021-09-24 23:53:42 -0400

    simple console rock paper scissors

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,52 @@
 
-console.log("hello, world!");
+let playerSelection = '';
+let computerSelection = '';
+let result = '';
+function humanPlay() {
+	let answer = prompt("type Rock, Paper, or Scissors");
+	playerSelection = answer.toLowerCase();
+	console.log(playerSelection);
+	return(playerSelection);
+}
+function computerPlay() {
+	let randomNumber = Math.floor(Math.random() * 3);
+	if (randomNumber === 0) {
+		computerSelection = 'Rock';
+		return(computerSelection);
+	} else if (randomNumber === 1) {
+		computerSelection = 'Paper';
+		return(computerSelection);
+	}
+	else {
+		computerSelection = 'Scissors';
+		return(computerSelection);
+	}
+	console.log(computerSelection);
+}
+
+function game(playerSelection, computerSelection) {
+	if (playerSelection === 'rock' && computerSelection === 'Scissors') {
+		result = 'You Win! ' + playerSelection + ' beats ' + computerSelection;
+		return(result);
+	} else if (playerSelection === 'paper' && computerSelection === 'Rock') {
+
+		result = 'You Win! ' + playerSelection + ' beats ' + computerSelection;
+		return(result);
+	} else if (playerSelection === 'scissors' && computerSelection === 'Paper') {
+
+		result = 'You Win! ' + playerSelection + ' beats ' + computerSelection;
+		return(result);
+	} /*else if (playerSelection !== 'rock' || playerSelection !== 'paper' || playerSelection !== 'scissors') {
+		alert('You must choose Rock, Paper, or Scissors'); }*/	
+	else {
+		result = 'You Lose! ' + computerSelection + ' beats ' + playerSelection;
+		return(result);
+	}
+}
+
+function runIt() {
+	computerPlay();
+	humanPlay();
+	game(playerSelection, computerSelection);
+	alert(result);
+}
